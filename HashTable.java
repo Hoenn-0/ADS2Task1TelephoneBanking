5bc801c8c039f662645a093b30e980ccca24504b
Hoenn-0 <76573968+Hoenn-0@users.noreply.github.com>
2021-08-31 22:33:06 +0100

    Add files via upload

---
+++ HashTable.java
@@ -0,0 +1,14 @@
+package TelephoneBanking;
+
+public class HashTable
+{
+public String key;
+public double value;
+
+    public HashTable(String key, double value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+}
